47c374e88db4dfe88003193eb676bb9c7e488556
Justin Makaila <1638996+justinmakaila@users.noreply.github.com>
2025-04-24 10:36:36 -0400

    update docs

--- server_http/app.py
+++ server_http/app.py
@@ -1,9 +1,12 @@
 from asyncio import sleep
 import os
+from . import make_mock_open, CaptureOnCloseBuffer
 from typing import List
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Response
 from fastapi.websockets import WebSocket
 from iracing.iracing_service import IRacingService
+from unittest.mock import patch
+from pathlib import Path
 
 test_file = os.getenv("PYIRSDK_TEST_FILE", None)
 
@@ -13,8 +16,13 @@ client = IRacingService(
 
 app = FastAPI(title="iRacing Telemetry API", debug=True)
 
-@app.get("/telemetry")
-def telemetry(keys: List[str] = Query(..., description="List of telemetry keys to retrieve")):
+@app.get("/telemetry", responses={
+    200: {
+        "content": { "application/json": { "example": { "PlayerCarIdx": 18, "LapDistPct": 0 }}},
+        "description": "Telemetry data"
+    }
+})
+def telemetry(keys: List[str] = Query(..., description="List of telemetry keys to retrieve", example=["PlayerCarIdx", "LapDistPct"])):
     if not keys:
         raise HTTPException(status_code=400, detail="No keys provided")
     if not client.check_connection():
@@ -23,6 +31,25 @@ def telemetry(keys: List[str] = Query(..., description="List of telemetry keys t
     if not telemetry_data:
         raise HTTPException(status_code=500, detail="Failed to retrieve telemetry data")
     return telemetry_data
+
+dump_path = Path(__file__).parent / "dump_response.yml"
+
+@app.get("/dump", response_class=Response, responses={ 
+    200: { 
+        "content": { "application/x-yaml": { "example": dump_path.read_text() }},
+        "description": "Dump of telemetry data in YML format"
+    }
+})
+def dump():
+    if not client.check_connection():
+        raise HTTPException(status_code=503, detail="iRacing client is not connected")
+    
+    request_buffer = CaptureOnCloseBuffer()
+    with patch("builtins.open", make_mock_open(request_buffer)):
+        client.client.parse_to('dump_path')
+
+    return request_buffer.getvalue()
+    
 
 @app.websocket("/ws/telemetry")
 async def telemetry_websocket(websocket: WebSocket):
@@ -53,7 +80,11 @@ async def telemetry_websocket(websocket: WebSocket):
 
         await websocket.close()
 
-@app.get("/schema/telemetry")
+@app.get("/schema/telemetry", responses={
+    200: {
+        "description": "Telemetry JSON schema"
+    }
+})
 def telemetry_schema():
     """
     Returns the telemetry schema.
@@ -63,7 +94,11 @@ def telemetry_schema():
     
     return client.telemetry_schema
 
-@app.get("/schema/session")
+@app.get("/schema/session", responses={
+    200: {
+        "description": "Session JSON schema"
+    }   
+})
 def session_schema():
     """
     Returns the session schema.
